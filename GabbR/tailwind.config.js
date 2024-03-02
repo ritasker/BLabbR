59191a21434c186be8bc0b2c2cc383be8a5ac479
ritasker <richard@pier8software.co.uk>
2024-03-02 12:09:32 +0000

    Initial Project Setup

---
+++ GabbR/tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    './Components/Pages/**/*.razor',
+    './Components/Layout/**/*.razor'
+  ],
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
